5e5012f3160f778c6c950f108b1070aa1edfa355
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:37:01 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,41 @@
+'''
+Print Pattern - X
+Given an integer n (where n >= 0 ), print an "X" shaped pattern with n rows above and below a central character x. The pattern should use backslashes (\) and forward slashes (/) to form the "X" shape, with increasing spaces between them as they approach the center. There should be no spaces to the right of the pattern.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input
+
+    • A single integer n, representing the number of rows above and below the center of the "X".
+
+Output
+
+    • An "X" shaped pattern with n rows above and below a central x character, as described.
+
+Examples
+
+Input:
+
+0
+Output:
+
+x
+Input:
+
+1
+Output:
+
+\ /
+ x
+/ \
+Input:
+
+2
+Output:
+
+\   /
+ \ /
+  x
+ / \
+/   \
+'''
