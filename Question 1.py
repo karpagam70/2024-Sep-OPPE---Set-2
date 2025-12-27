64f03c2d3041edef34e10d8fcc50ef84d958b58d
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:25:56 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,18 @@
+'''
+Check Positive Odd or Negative Even
+Write a function is_positive_odd_or_negative_even that checks if an integer is either a positive odd number or a negative even number. Return True if the number meets one of these conditions; otherwise, return False.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example cases:
+
+    • 0 --> False (neither positive nor negative)
+
+    • 3 --> True (positive odd)
+
+    • -4 --> True (negative even)
+
+    • 4 --> False (positive but even)
+
+    • -3 --> False (negative but odd)
+'''
