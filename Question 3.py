19078e84ab1e627a3de07bcaa0162c46b8cf7a41
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:32:38 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,12 @@
+'''
+Replace middle with n times middle
+Given a tuple t of odd length, and a positive integer n, replace the middle element with n copies of itself.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+For t = (1, 2, 3, 4, 5) and n = 5, the result should be (1, 2, 3, 3, 3, 3, 3, 4, 5).
+
+For t = (1, 2, 3) and n = 4, the result should be (1, 2, 2, 2, 2, 3).
+'''
