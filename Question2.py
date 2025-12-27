5e7bed077487fc6c75d05ad137be030f15af01a0
karpagam70 <karpagam1332000@gmail.com>
2025-12-27 15:30:17 +0530

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,16 @@
+'''
+Check if the Given String is Within Quotes and Has Quotes Inside
+Write a function within_and_has_double_quotes that checks if a given string starts and ends with double quotes, and if there are double quotes inside the string (excluding the first and last characters). Return True if both conditions are met, and False otherwise.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example cases:
+
+    • "abcd"efgh" --> True (starts and ends with double quotes, and has double quotes inside)
+
+    • 'abcd"efgh"' --> False (does not start with double quotes)
+
+    • "'abcd'efgh'" --> False (uses single quotes)
+
+    • "abcdefgh" --> False (no double quotes inside the quotes)
+'''
